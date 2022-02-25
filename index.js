c4071bef4cabf33e8e3de2dc627503194d0dab74
Alain Giessel <alain.giessel@gmail.com>
2022-02-24 22:00:46 -0700

    commit tree 25fe73d8382524680637e2846f72d75c87de6c58

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+/* const clients = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCard(clients) {
+    for (let i = 0; i < clients.length; i++) {
+        console.log(`Thank you, ${clients[i]}, for the wonderful surprise gift!`);
+    }
+    return clients;
+}
+
+writeCard(clients)
+*/
+const gifts = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(gifts) {
+    let i = 0;
+    while (i < gifts.length) {
+        console.log(`Thank you, ${gifts[i]}, for the wonderful surprise gift!`);
+        i++;
+    }
+    return gifts;
+}
+writeCards(gifts);
+
+function countdown() {
+    let countdown = 10;
+    while (countdown >= 0) {
+        console.log(countdown--);
+    }
+}
+console.log(countdown);
